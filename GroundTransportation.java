d2790f84fb115992549bb84eeada5f7dd4e5fcc2
RodrigoNunes-202100224 <202100224@estudantes.ips.pt>
2024-04-04 10:23:48 +0100

    Nível 1

---
+++ GroundTransportation.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class GroundTransportation here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GroundTransportation extends Transport
+{
+    private String licensePlate;
+
+    public GroundTransportation()
+    {
+        super();
+        setFees(3.0);
+    }
+}
